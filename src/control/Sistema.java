cf3e6ee66bc1902a055b3ba469455289b3ee1a07
diegoDiaz37 <diego.diaz5671@gmail.com>
2024-04-11 13:23:50 +0200

    0

---
+++ src/control/Sistema.java
@@ -0,0 +1,12 @@
+package control;
+
+public class Sistema {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Diego
+ *
+ */
+module ProyectoProyectoIngenieria {
+}
